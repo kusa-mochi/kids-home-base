285a800b0b93045b946e51212b3c504513971e8e
Kusa Mochi <whoatemyapplepie@gmail.com>
2026-08-08 21:49:42 +0900

    計画データ追加、定期計画データ追加のAPIハンドラを実装

--- src/backend/api_handlers/addRecurringSchedule.go
+++ src/backend/api_handlers/addRecurringSchedule.go
@@ -1,1 +1,21 @@
 package api_handlers
+
+import (
+	"kids_home_base/commands"
+	datastructures "kids_home_base/data_structures"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AddRecurringScheduleHandler(c *gin.Context, apiRequest chan commands.ICommand) {
+	recurringScheduleItem := &datastructures.RecurringScheduleItem{}
+	if err := c.ShouldBindJSON(recurringScheduleItem); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request body"})
+		return
+	}
+
+	cmd := commands.NewAddRecurringScheduleItemCommand(recurringScheduleItem)
+	apiRequest <- cmd
+	response := <-cmd.Response
+	c.JSON(200, response)
+}
--- src/backend/api_handlers/addScheduleItem.go
+++ src/backend/api_handlers/addScheduleItem.go
@@ -1,1 +1,21 @@
 package api_handlers
+
+import (
+	"kids_home_base/commands"
+	datastructures "kids_home_base/data_structures"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AddScheduleItemHandler(c *gin.Context, apiRequest chan commands.ICommand) {
+	scheduleItem := &datastructures.ScheduleItem{}
+	if err := c.ShouldBindJSON(scheduleItem); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request body"})
+		return
+	}
+
+	cmd := commands.NewAddScheduleItemCommand(scheduleItem)
+	apiRequest <- cmd
+	response := <-cmd.Response
+	c.JSON(200, response)
+}
