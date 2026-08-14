2345e8e85ffe45f4282cb814c65d725137334caa
Kusa Mochi <whoatemyapplepie@gmail.com>
2026-08-14 22:33:11 +0900

    今日の予定画面のワイヤーフレームを実装。データ表示も実装済。

--- src/frontend/app/pages/TodaySchedule.tsx
+++ src/frontend/app/pages/TodaySchedule.tsx
@@ -1,6 +1,38 @@
 "use client";
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useTodaySchedule } from "../contexts/TodayScheduleContext";
+import { ScheduleResponse } from "../dataStructures/Schedule";
 
 export const TodaySchedule: FC = () => {
-  return <div>Today Schedule</div>;
+  const { todaySchedule, setTodaySchedule } = useTodaySchedule();
+  
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/get-today-schedule`)
+      .then((response) => response.json())
+      .then((data: ScheduleResponse) => {
+        setTodaySchedule({ items: data.schedules ?? [] });
+      })
+      .catch((error) => {
+        console.error("Error fetching today's schedule:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          {todaySchedule.items.map((item, index) => {
+            const itemDate = new Date(item.dt);
+            const itemTime = itemDate.toLocaleTimeString("ja-JP", { hour: "2-digit", minute: "2-digit" });
+            return (
+              <tr key={index}>
+                <td>{itemTime}</td>
+                <td>{item.task}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
