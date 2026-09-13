c810127789d703b337ca66fbc518b21d69a71847
Kusa Mochi <whoatemyapplepie@gmail.com>
2026-09-13 16:24:09 +0900

    UWPアプリ化

--- src/frontend/app/layout.tsx
+++ src/frontend/app/layout.tsx
@@ -43,6 +43,13 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="てつてつ" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body style={{ width: '100%', height: '100%' }}>
         <ComposeProviders providers={providers}>
           {children}
